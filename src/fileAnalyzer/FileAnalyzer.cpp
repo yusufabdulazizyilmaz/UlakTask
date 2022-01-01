9481fd0c812ab98e7072694fd8e6314bebf1b4e9
yusufabdulazizyilmaz <yusufabdulazizyilmaz94@gmail.com>
2022-01-01 13:47:54 +0300

    build: add project to github

---
+++ src/fileAnalyzer/FileAnalyzer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SPAK on 1.01.2022.
+//
+
+#include "FileAnalyzer.h"
---
+++ src/fileAnalyzer/FileAnalyzer.h
@@ -0,0 +1,8 @@
+//
+// Created by SPAK on 1.01.2022.
+//
+
+#ifndef ULAK_FILEANALYZER_H
+#define ULAK_FILEANALYZER_H
+
+#endif //ULAK_FILEANALYZER_H
