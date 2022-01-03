efbcd4ef27b7f73720515505e521bc0a680c1ec9
yusufabdulazizyilmaz <92329809+yusufabdulazizyilmaz@users.noreply.github.com>
2022-01-03 16:55:44 +0300

    Test: add LibaryIndexerTest

---
+++ Google_tests/LibaryIndexerTest.cpp
@@ -0,0 +1,22 @@
+
+//
+// Created by yusufyilmaz on 3.01.2022.
+//
+#include "gtest/gtest.h"
+#include "src/indexer/LibraryIndexer.h"
+#include "src/fileAnalyzer/FileAnalyzer.h"
+TEST(LibraryIndexerTest,getHashStrMap){
+    //Arrange
+    FileAnalyzer fileAnalyzer{"/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt"};
+    LibraryIndexer inversedIndex{fileAnalyzer};
+    std::unordered_map<std::string, std::map<std::string, int>> expected;
+    for (const auto& word : {"name","yusuf","selami"})
+        for (const auto& txtfile : {"a1/a3.txt","a1.txt","a2/a21/a3.txt","a3/a2.txt"})
+            expected[word][txtfile]=2;
+    //Act
+    auto hashmap =inversedIndex.getInversedIndex();
+    //Assert
+    for (const auto [word,map] : hashmap)
+        for (const auto [txtfile,count] : map)
+            ASSERT_EQ(hashmap[word][txtfile],expected[word][txtfile]);
+}
