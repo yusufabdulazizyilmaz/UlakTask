806681b2d2c6c577bacbc2160c211b3eaaac555b
yusufabdulazizyilmaz <abdulazizyilmaz94@gmail.com>
2022-01-04 11:38:54 +0300

    Test: add LibraryIndexerTest module

--- Google_tests/FileAnalyzerTest.cpp
+++ Google_tests/FileAnalyzerTest.cpp
@@ -4,53 +4,44 @@
 #include "gtest/gtest.h"
 #include "src/fileAnalyzer/FileAnalyzer.h"
 
-TEST(FileAnalyzer, getlibrary)
+std::string filePath()
 {
-    // Arrange
     std::stringstream ss;
     ss << ROOT_PATH << static_cast<char>(std::filesystem::path::preferred_separator) << "data"
        << static_cast<char>(std::filesystem::path::preferred_separator) << "testtxt";
-    FileAnalyzer fileAnalyzer(ss.str());
-    // Act
-    std::string value = fileAnalyzer.getLibrary();
-    // Assert
-    ASSERT_STREQ(value.c_str(), strdup(ss.str().c_str()));
+    return ss.str();
 }
 
-/*TEST(FileAnalyzerTest, getTxtVector)
+struct FileAnalyzerTest : public testing::Test {
+    std::unique_ptr<FileAnalyzer> fileAnalyzer_uptr;
+    FileAnalyzerTest():fileAnalyzer_uptr {new FileAnalyzer{filePath()}} {}
+};
+
+TEST_F(FileAnalyzerTest, getlibrary)
 {
     // Act
-    /*std::vector<std::string> expectedVec{"/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a1/a2.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a1/a3.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a1/za1.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a1/za4.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a1.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a1.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a2.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a21/a1.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a21/a2.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a21/a3.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a21/a4.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a3.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a2/a4.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a3/a1.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a3/a2.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a3/a3.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a3/a4.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a3.txt",
-    "/home/yusufyilmaz/Desktop/yusufgithub/UlakTask-firstbranch/data/testtxt/a4.txt"};
+    std::string value = fileAnalyzer_uptr->getLibrary();
+    // Assert
+    ASSERT_STREQ(value.c_str(), filePath().c_str());
+}
 
-    std::vector<std::string> expectedVec{"C:\\Users\\SPAK\\UlakTask\\data\\testtxt\\a1/a3.txt",
-                                         "C:\\Users\\SPAK\\UlakTask\\data\\testtxt\\a1.txt",
-                                         "C:\\Users\\SPAK\\UlakTask\\data\\testtxt\\a2\\a21\\a3.txt",
-                                         "C:\\Users\\SPAK\\UlakTask\\data\\testtxt\\a3\\a2.txt"};
-
+TEST_F(FileAnalyzerTest, getTxtVector)
+{
+    // Arrange
+    std::stringstream ss1,ss2,ss3,ss4;
+    char seperator = static_cast<char>(std::filesystem::path::preferred_separator);
+    ss1 << filePath() << seperator << "a1" << seperator << "a3.txt";
+    ss2 << filePath() << seperator << "a1.txt";
+    ss3 << filePath() << seperator << "a2" << seperator << "a21" << seperator << "a3.txt";
+    ss4 << filePath() << seperator << "a3" << seperator << "a2.txt";
+    std::vector<std::string> expectedVec{ ss1.str(), ss2.str(), ss3.str(), ss4.str()};
+    // Act
     auto txtFileVec = fileAnalyzer_uptr->getTxtFileVec();
     sort(begin(txtFileVec), end(txtFileVec));
     std::vector<std::string> results;
-    transform(begin(txtFileVec), end(txtFileVec), std::back_inserter(results),
+    transform(begin(txtFileVec), end(txtFileVec), back_inserter(results),
             [](const auto& path) { return path.string(); });
     // Assert
     EXPECT_TRUE(std::equal(begin(results), end(results), begin(expectedVec), end(expectedVec)));
-}*/
+}
 
--- Google_tests/LibaryIndexerTest.cpp
+++ Google_tests/LibaryIndexerTest.cpp
@@ -6,14 +6,23 @@
 #include "src/indexer/LibraryIndexer.h"
 #include "src/fileAnalyzer/FileAnalyzer.h"
 
-/*TEST(LibraryIndexerTest, getHashStrMap)
+TEST(LibraryIndexerTest, getHashStrMap)
 {
     //Arrange
-    FileAnalyzer fileAnalyzer{"C:\\Users\\SPAK\\UlakTask\\data\\testtxt"};
+    std::stringstream ss;
+    char seperator = static_cast<char>(std::filesystem::path::preferred_separator);
+    ss << ROOT_PATH << seperator << "data" << seperator << "testtxt";
+    FileAnalyzer fileAnalyzer{ss.str()};
     LibraryIndexer inversedIndex{fileAnalyzer};
+
+    std::stringstream ss1,ss2,ss3,ss4;
+    ss1 << "a1" << seperator << "a3.txt";
+    ss2 << "a1.txt";
+    ss3 << "a2" << seperator << "a21" << seperator << "a3.txt";
+    ss4 << "a3" << seperator << "a2.txt";
     std::unordered_map<std::string, std::map<std::string, int>> expected;
     for (const auto& word: {"name", "yusuf", "selami"})
-        for (const auto& txtfile: {"a1\\a3.txt", "a1.txt", "a2\\a21\\a3.txt", "a3\\a2.txt"})
+        for (const auto& txtfile: {ss1.str(), ss2.str(), ss3.str(), ss4.str()})
             expected[word][txtfile] = 2;
     //Act
     auto hashmap = inversedIndex.getInversedIndex();
@@ -21,4 +30,4 @@
     for (const auto[word, map]: hashmap)
         for (const auto[txtfile, count]: map)
             ASSERT_EQ(hashmap[word][txtfile], expected[word][txtfile]);
-}*/
+}
